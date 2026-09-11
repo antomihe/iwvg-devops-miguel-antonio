3a93188638a51226c28ba22f973861f1dadb6d6a
Antonio Miguel Herrero <antomihe@gmail.com>
2026-09-11 19:51:14 +0200

    add GET /user/{id} endpoint with Docker environment setup. Ref #5.

--- src/main/java/es/upm/miw/devops/Application.java
+++ src/main/java/es/upm/miw/devops/Application.java
@@ -3,8 +3,10 @@ package es.upm.miw.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
+@EnableJpaRepositories(basePackages = "es.upm.miw.devops.data.daos")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);// mvn clean spring-boot:run
