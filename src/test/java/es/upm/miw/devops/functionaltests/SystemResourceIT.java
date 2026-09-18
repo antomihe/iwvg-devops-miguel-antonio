0539c6e1d10c1c7024d611904c596ad6ecb8547f
Antonio Miguel Herrero <antomihe@gmail.com>
2026-09-18 19:46:04 +0200

    update SystemResource and functional tests. Ref #23

--- src/test/java/es/upm/miw/devops/functionaltests/SystemResourceIT.java
+++ src/test/java/es/upm/miw/devops/functionaltests/SystemResourceIT.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -17,13 +21,31 @@ class SystemResourceIT {
     private WebTestClient webTestClient;
 
     @Test
-    void testReadVersion() {
+    void testApplicationInfo() {
         this.webTestClient
                 .get()
-                .uri(SystemResource.SYSTEM + SystemResource.VERSION)
+                .uri("/")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
-                .isEqualTo(SystemResource.VERSION_BADGE);
+                .value(response -> {
+                    assertNotNull(response);
+                    assertTrue(response.contains("/version-badge"));
+                });
+    }
+
+    @Test
+    void testGenerateBadge() {
+        this.webTestClient
+                .get()
+                .uri(SystemResource.VERSION_BADGE)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.valueOf("image/svg+xml"))
+                .expectBody(byte[].class)
+                .value(bytes -> {
+                    assertNotNull(bytes);
+                    assertTrue(bytes.length > 0);
+                });
     }
 }
