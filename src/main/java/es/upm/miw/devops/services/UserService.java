6ec1dd5d52f9c0bb3288cd108eeac413bc18a3e9
Antonio Miguel Herrero <antomihe@gmail.com>
2026-09-19 09:11:27 +0200

    Added endpoint: PUT /user/{id}. Ref. #45

--- src/main/java/es/upm/miw/devops/services/UserService.java
+++ src/main/java/es/upm/miw/devops/services/UserService.java
@@ -18,6 +18,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    // MOCK COMMIT - THIS WOULD BE ADDED IN REGULAR DEVELOP
     public User create(User user) {
         this.assertMobileNotExist(user.getMobile());
         user.setId(null);
@@ -33,6 +34,7 @@ public class UserService {
                 .orElseThrow(() -> new NotFoundException("User id not found: " + id));
     }
 
+    // MOCK COMMIT - THIS WOULD BE ADDED IN REGULAR DEVELOP
     public User update(UUID id, User user) {
         User existingUser = this.read(id);
         if (!existingUser.getMobile().equals(user.getMobile())) {
